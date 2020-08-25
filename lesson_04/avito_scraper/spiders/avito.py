b04c56c529645784555c7b4fe4338277f670f022
sab1e <antony.sobolev@gmail.com>
2020-08-25 06:49:17 +0500

    add method for phone parse

--- lesson_04/avito_scraper/spiders/avito.py
+++ lesson_04/avito_scraper/spiders/avito.py
@@ -18,7 +18,8 @@ class AvitoSpider(scrapy.Spider):
     def parse(self, response, start=True):
         if start:
             pages_count = int(
-                response.xpath(self.__xpath_query['pagination']).extract()[-1].split('(')[-1].replace(')', ''))
+                response.xpath(self.__xpath_query['pagination']). \
+                    extract()[-1].split('(')[-1].replace(')', ''))
 
             for num in range(2, pages_count + 1):
                 yield response.follow(
@@ -31,7 +32,6 @@ class AvitoSpider(scrapy.Spider):
             yield response.follow(
                 link,
                 callback=self.ads_parse
-
             )
 
     def ads_parse(self, response):
@@ -42,14 +42,30 @@ class AvitoSpider(scrapy.Spider):
             'ul.gallery-list.js-gallery-list '
             'img::attr("src")').extract()
         params_names = response.css('ul.item-params-list '
-                                   'li.item-params-list-item '
-                                   'span::text').extract()
+                                    'li.item-params-list-item '
+                                    'span::text').extract()
         params_values = response.css('div.item-params '
                                      'ul.item-params-list '
                                      'li::text').extract()
         address = response.css('div.item-address '
                                'span.item-address__string::text').extract()
+        price = response.xpath(
+            '//div[@id="price-value"]/'
+            'span[contains(@class, "price-value-string")]/'
+            'span[@class="js-item-price"]/@content').extract_first()
 
+        cash_sign = response.xpath(
+            '//div[@id="price-value"]/'
+            'span[contains(@class, "price-value-string")]//'
+            'span[@class="font_arial-rub"]/text()').extract_first()
 
+        print(1)
+        yield response.follow(
+                'https://m.avito.ru/tyumen/kvartiry/tel:phone',
+                headers={
+                    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Mobile Safari/537.36'},
+                callback=self.phone_parse
+            )
 
+    def phone_parse(self, response):
         print(1)
